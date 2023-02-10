a7ddfe842b02805e371e577de00370c582d213d3
Abdul Hannan <comsianabdulhannan@gmail.com>
2023-02-11 00:08:34 +0500

    completed lab 99 lines of code

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
 let friend=["John", "Maxwell","johnson","David","Peterson"];
+for(let i=0; i<friend.length; i++)
+{
+    console.log(friend[i]);
+    for(let j=99;j>=0;j--)
+    {
+        console.log(j+ " lines of code");
+    }
+    
+}
+
+
+
+   
+    
 
