bb8edbc575380907b9c905dee71888dcb87d4972
Abdul Hannan <88274981+Dev-AbdulHannan@users.noreply.github.com>
2023-02-11 15:12:11 +0500

    Revert "Remove the console expressions"

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 let friend=["John", "Maxwell","johnson","David","Peterson"];
 for(let i=0; i<friend.length; i++)
 {
-    
+    console.log(friend[i]);
     for(let j=99;j>=0;j--)
     {
-        
+        console.log(j+ " lines of code");
     }
     
 }
